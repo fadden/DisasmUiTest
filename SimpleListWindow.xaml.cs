fda3796ef143a406fed4ff35b9c1b929f4d73ff2
Andy McFadden <fadden@fadden.com>
2019-05-18 13:57:51 -0700

    Tweak project for style extraction
    The easiest way to get the current ListView styles is to extract
    them from the designer.

--- SimpleListWindow.xaml.cs
+++ SimpleListWindow.xaml.cs
@@ -1,27 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using System.Windows.Markup;
 
-namespace DisasmUiTest
-{
+namespace DisasmUiTest {
     /// <summary>
     /// Interaction logic for SimpleListWindow.xaml
     /// </summary>
-    public partial class SimpleListWindow : Window
-    {
-        public SimpleListWindow()
-        {
+    public partial class SimpleListWindow : Window {
+        public SimpleListWindow() {
             InitializeComponent();
+
+#if false
+            // Add a button that captures the style.  Probably easier to just use the approach
+            // from https://stackoverflow.com/a/38875063/294248 (especially since I can't
+            // figure out how to extract a ListViewItem's style).
+            Button bt = new Button();
+            bt.Name = "CaptureStyle";
+            bt.Content = "Capture Style";
+            bt.Click += CaptureStyle_Click;
+            rootPanel.Children.Add(bt);
+#endif
+        }
+
+        private void CaptureStyle_Click(object obj, EventArgs e) {
+            string outFile = @"C:\Src\template.xaml";
+            string xaml;
+
+            xaml = XamlWriter.Save(codeListView.Template); // template for ListView obj
+
+            //xaml = XamlWriter.Save(codeListView.ItemTemplate); // null
+            //xaml = XamlWriter.Save(codeListView.ItemContainerStyle); // null
+
+            File.WriteAllText(outFile, xaml);
+            Debug.WriteLine("Written to '" + outFile + "'");
         }
     }
 }
